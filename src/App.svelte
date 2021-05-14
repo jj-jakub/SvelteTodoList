2dbdae86bab73e617002f6f78ce05bc4eca95ef1
jj <>
2021-05-14 10:08:20 +0200

    Create Svelte app template

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let name;
+import TabContainer from './components/tabs/TabContainer.svelte';
+	import './theme/app.css'
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<TabContainer/>
+</main>
