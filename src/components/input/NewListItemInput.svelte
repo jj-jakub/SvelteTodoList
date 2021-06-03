05a78e26b355c5634aff0c77907c18360c23dc2e
jj <>
2021-06-03 20:55:30 +0200

    Rearrange styles v2

--- src/components/input/NewListItemInput.svelte
+++ src/components/input/NewListItemInput.svelte
@@ -16,3 +16,13 @@
     <input id="itemTextInput">
     <button on:click={onAddButtonClick}>Add item</button>
 </div>
+
+<style>
+    input, button {
+        padding: 0 10px 0 10px;
+        list-style-type: none;
+        margin: 24px 0 0 32px;
+        font-size: 18px;
+        color: #555;
+    }
+</style>
